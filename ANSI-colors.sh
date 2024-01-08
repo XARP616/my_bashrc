4044576495796f30e3ebad4f031166b5e2785bb9
León <72978970+XARP616@users.noreply.github.com>
2024-01-08 18:18:49 +0100

    ANSI colors SH

---
+++ ANSI-colors.sh
@@ -0,0 +1,26 @@
+# ANSI COLORS
+## Primary
+color0='\033[0;30m'
+color1='\033[0;31m'
+color2='\033[0;32m'
+color3='\033[0;33m'
+color4='\033[0;34m'
+color5='\033[0;35m'
+color6='\033[0;36m'
+color7='\033[0;37m'
+
+## Secondary (strong)
+color0s='\033[1;30m'
+color1s='\033[1;31m'
+color2s='\033[1;32m'
+color3s='\033[1;33m'
+color4s='\033[1;34m'
+color5s='\033[1;35m'
+color6s='\033[1;36m'
+color7s='\033[1;37m'
+
+## Reset color
+nc='\033[0m'
+
+#printf "I ${color1}love${nc} Stack Overflow\n"
+## Credit: https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux
